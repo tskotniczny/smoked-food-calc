20f4235c411d9a235d385c1acb7631404148f212
Tomek <tskotniczny1@gmail.com>
2020-11-29 11:16:52 +0000

    Add onClick function to add new component each time button is clicked

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 import Meat from './Meat';
 
-
 class App extends Component{
 
+    state = { children: []}
+
+    addMeatComponent = () => {
+        this.setState({children: [<Meat />, this.state.children]});
+        
+    }
+
+
+
     render() {
+
+        console.log('count: ', this.state.counter);
 
         return (
             <div>
                 <h1>Kalkulator do wedzenia</h1>
-
-                {/* addnewComponent component */ }
                 <div>
-                    <button className='addNewComponent' style={{width: 100, height: 100 }}>Dodaj mieso, ser lub ryby</button>
+                    <button style={{padding: 100}} className='addComponent' onClick={this.addMeatComponent} style={{width: 100, height: 100 }}>Dodaj mieso, ser lub ryby</button>
+                    <button style={{margin: 100}} className='addComponent' onClick={this.addMeatComponent} style={{width: 100, height: 100 }}>Dodaj mieso, ser lub ryby</button>
+                    <button style={{margin: 10}} className='addComponent' onClick={this.addMeatComponent} style={{width: 100, height: 100 }}>Dodaj mieso, ser lub ryby</button>
                 </div>
-                <Meat />
+                <div className='meatContainer'>
+                   
+                    {this.state.children.map(child => child)}
+                </div>
+                
+                
 
             </div>
         )
